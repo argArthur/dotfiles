b0b1dbfc3263608ec6a75b6780d2157645236d14
Arthur Alves Ribeiro Gordo <arthurarg100@gmail.com>
2025-08-09 03:17:19 -0300

    add setup script

---
+++ setup.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ln -s "$(pwd)/nvim" "$HOME/.config"
+ln -s "$(pwd)/tmux" "$HOME/.config"
+ln -s "$(pwd)/.tmux.conf" "$HOME"
+ln -s "$(pwd)/fish" "$HOME/.config"
+ln -s "$(pwd)/ghostty" "$HOME/.config"
+
